ea83e12b8e78819417bd123a3433bbdb073e95db
vishal <mauryavishal767@gmail.com>
2024-04-03 23:11:22 +0530

    girli project

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// preloaderS
+const preLoad = document.querySelector(".preLoader");
+window.addEventListener("load",function() {
+    preLoad.style.display = "none";
+})
+
+// on scrolling top bar disapper 
+const topBar = document.querySelector('.topBar');
